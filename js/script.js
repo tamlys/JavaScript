177e5d4a9598180ad67a762311bc478ae5560e87
Maxim <oqiwhite1996@gmail.com>
2021-04-12 14:39:17 +0300

    домашнее задание - 4

--- js/script.js
+++ js/script.js
@@ -1,44 +1,84 @@
 "use strict";
 
-const options = {
-    name : "Square",
-    width: 1024,
-    height: 1024,
-    colors : {
-        border: 'black',
-        background: 'red'
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+let numberOfFilms;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+        }
     },
-    makeTest: function() {
-        console.log("Test Function");
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
+                  rating = prompt('На сколько оцените его?', '');
+            if (lastMovie != null && rating != null && lastMovie != '' && rating != '' && lastMovie.length < 50) {
+                personalMovieDB.movies[lastMovie] = rating;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if(!hidden) {
+            console.log(personalMovieDB);
+        }
+    }, 
+    writeYourGenres: function() {
+        for(let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre != null && genre.trim() != '') {
+                personalMovieDB.genres[i - 1] = genre;
+            } else {
+                i--;
+            }
+        }
+        personalMovieDB.genres.forEach(function(item, index, genres) {
+            console.log(`Любимый жанр #${++index} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat === false) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
     }
 };
 
-// console.log(options.name);
-// console.log("Background color:", options.colors.border);
-
-// delete options.colors.background;
-// console.log("Background color:", options.colors.background);
-
-// options.colors.background = "green";
-// console.log("New Background color:", options.colors.background);
-
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Property ${i} is ${options[key][i]}`);
-//             counter++;
-//         }
-//     } else {
-//         console.log(`Property ${key} is ${options[key]}`);
-//         counter++;
-//     }
-// }
-// console.log(counter);
-
-// console.log(Object.keys(options).length);
-
-// options.makeTest();
-
-const {border, background} = options.colors;
-console.log(background);
+//personalMovieDB.start();
+//personalMovieDB.rememberMyFilms();
+//personalMovieDB.detectPersonalLevel();
+//personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
+//personalMovieDB.toggleVisibleMyDB();
