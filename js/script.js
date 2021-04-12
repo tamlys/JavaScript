0620bd10d560e8c50b6b260f16772da71ce0a3a5
Maxim <oqiwhite1996@gmail.com>
2021-04-12 16:24:55 +0300

    Динамическая типизация

--- js/script.js
+++ js/script.js
@@ -1,84 +1,39 @@
 "use strict";
 
-/* Задание на урок:
+// To String
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+console.log(typeof(5 + ''));
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+const num = 5;
+console.log("https://yandex.ru/catalog/" + num);
 
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+const fontSize = 26 + 'px';
 
-let numberOfFilms;
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-    
-        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-        }
-    },
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
-                  rating = prompt('На сколько оцените его?', '');
-            if (lastMovie != null && rating != null && lastMovie != '' && rating != '' && lastMovie.length < 50) {
-                personalMovieDB.movies[lastMovie] = rating;
-            } else {
-                i--;
-            }
-        }
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            console.log("Просмотрено довольно мало фильмов");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log("Вы классический зритель");
-        } else if (personalMovieDB.count >= 30) {
-            console.log("Вы киноман");
-        } else {
-            console.log("Произошла ошибка");
-        }
-    },
-    showMyDB: function(hidden) {
-        if(!hidden) {
-            console.log(personalMovieDB);
-        }
-    }, 
-    writeYourGenres: function() {
-        for(let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            if (genre != null && genre.trim() != '') {
-                personalMovieDB.genres[i - 1] = genre;
-            } else {
-                i--;
-            }
-        }
-        personalMovieDB.genres.forEach(function(item, index, genres) {
-            console.log(`Любимый жанр #${++index} - это ${item}`);
-        });
-    },
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat === false) {
-            personalMovieDB.privat = true;
-        } else {
-            personalMovieDB.privat = false;
-        }
-    }
-};
+// To Number
+console.log(typeof(Number('4')));
 
-//personalMovieDB.start();
-//personalMovieDB.rememberMyFilms();
-//personalMovieDB.detectPersonalLevel();
-//personalMovieDB.showMyDB(personalMovieDB.privat);
-personalMovieDB.writeYourGenres();
-//personalMovieDB.toggleVisibleMyDB();
+console.log(typeof(+'4'));
+
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt("Input number", "");
+
+// To Boolean
+0, '', null, undefined, NaN
+
+let switcher = null;
+if (switcher) {
+    console.log("Working...");
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log("Working...");
+}
+
+console.log(typeof(Boolean('4')));
+
+console.log(typeof((!!'4')));
