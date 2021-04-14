b56e4aaa52ccbb0059557e453caed07e3218d995
Maxim <oqiwhite1996@gmail.com>
2021-04-14 12:02:41 +0300

    События и их обработчики

--- js/script.js
+++ js/script.js
@@ -1,54 +1,35 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+const btn = document.querySelector("button"),
+      btns = document.querySelectorAll("button"), 
+      overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//     alert('Click');
+// };
+
+// let i = 0;
+const deleteElement = (e) => {
+    // e.target.remove();
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
 };
 
-const promo = document.querySelectorAll('.promo__adv img');
-const genre = document.querySelector(".promo__genre");
-const poster = document.querySelector(".promo__bg");
-const filmList = document.querySelector(".promo__interactive-list");
 
-//1
-promo.forEach(item => {
-    item.remove();
-});
-//2
-genre.textContent = "Драма";
-//3
-poster.style.background = "url('../img/bg.jpg')";
-//4
-filmList.innerHTML = "";
-
-movieDB.movies.sort();
-movieDB.movies.forEach((item, index) => {
-    filmList.innerHTML += `
-    <li class="promo__interactive-item">${++index}. ${item}
-        <div class="delete"></div>
-    </li>`;
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
 });
 
+// btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement);
 
-
-
+const link = document.querySelector('a');
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+});
 
