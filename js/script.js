8a02d691f01965863639b87e03e4ee5f8dad838f
Maxim <oqiwhite1996@gmail.com>
2021-04-15 10:36:32 +0300

    Async, defer, динамические скрипты

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+const p = document.querySelectorAll('p');
+console.log(p);
 
-    box.addEventListener('touchstart', (event) => {
-        event.preventDefault();
-        console.log('Start');
-        console.log(event.changedTouches);
-    });
 
-    box.addEventListener('touchmove', (event) => {
-        event.preventDefault();
-        console.log(event.targetTouches[0].pageX);
-    });
 
-    // box.addEventListener('touchend', (event) => {
-    //     event.preventDefault();
-    //     console.log('End');
-    // });
-});
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false; 
+    document.body.append(script);
+}
 
-//touches, targetTouches, changedTouches
+loadScript("js/test.js");
+loadScript("js/some.js");
 
+
+// defer - не блокирует страницу, дойдя до скрипта,
+// продолжает загружать страницу и в фоновом режиме грузит скрипт.
+// Скрипт начнет выполняться только тогда, когда будет загружено DOM дерево,
+// если js файлов несколько, то выполняются последовательно
+
+// async - страница не ждет скрипт. Содержимое обрабатывается и отображается
+// DOMContentLoaded и async скрипты не ждут друг друга
+// какой файл скрипта быстрее прогрузился(фон. реж), тот и заработал первым
+// Применяется к скриптам, которые не зависят от DOM структуры +
+// не должен зависить от других скриптов
+
+// Динамически загружаемые скрипты ведут себя также, как async.
+// С помощью script.async = false будет вести себя как и обычный скрипт
