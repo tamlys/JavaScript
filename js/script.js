2e09111c4b441aa7ee9ad9e4efe5f9b8e8a49aa6
Maxim <oqiwhite1996@gmail.com>
2021-04-22 11:27:33 +0300

    Контекст вызова. This

--- js/script.js
+++ js/script.js
@@ -141,33 +141,97 @@
 //     window.addEventListener('scroll', showModalByScroll);
 // });
 
-// Constructor
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this)
+        return a + b;
+    }
+    console.log(sum());
+}
+
+showThis(4, 5);
+
+const obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();
+
 function User(name, id) {
     this.name = name;
     this.id = id;
     this.human = true;
     this.hello = function() {
-        console.log(`Hello ${this.name}`);
+        console.log('Hello! ' + this.name);
     };
 }
 
-const ivan = new User('Ivan', 28);
+let ivan = new User('Ivan', 23);
 
-// ivan.exit();  Вызов до объявления метода, не сработает!
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`);
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + ' ' + surname);
+}
+
+const user = {
+    name: 'John'
 };
 
-const alex = new User('Alex', 20);
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
 
-alex.hello();
-ivan.hello();
+function count(num) {
+    return this*num;
+}
 
-alex.exit();
-ivan.exit();
+const double = count.bind(2);
+console.log(double(3));
 
-console.log(ivan);
-console.log(alex);
+const btn = document.querySelector('button');
 
+btn.style.width = '100px';
+btn.style.height = '50px';
+btn.style.display = 'block';
+btn.style.margin = '0 auto';
 
+btn.addEventListener('click', function() {
+// btn.addEventListener('click', (e) => {
+    this.style.background = 'red';
+    // e.target.style.background = 'red';
+    console.log(this);
+    // Обращается к элементу на котором происходит событие - button.
+    // Если будем использовать стрелочную функцию - window
+});
+
+const object = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this);
+        };
+        say();
+    }
+};
+
+object.sayNumber(); // Стрелочная функция не имеет контекста 
+                //- будет указывать на контекст родителя
+
+const dbl = a => a * 2;
+
+console.log(dbl(4));
+
+/*
+ 1) Обычная функция: this = window, но если use strict - undefined
+ 2) Контекст у методов объекта - сам объект
+ 3) this в конструкторах и классах, это новый экземпляр объекта
+ 4) Ручная привязка this: call, apply, bind
+*/
 
